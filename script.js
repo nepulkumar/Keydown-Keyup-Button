77baf2f809c24ce657bfb57603e20bfe30f9761f
nepulkumar <124512972+nepulkumar@users.noreply.github.com>
2023-10-02 23:07:46 +0530

    Add files via upload
    Final Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// keydown and keyup event
+
+//keydown
+
+let container = document.getElementById("container");
+let display = document.getElementById("display");
+
+//adding a keydown event listener to the document
+document.addEventListener("keydown", function (e) {
+    display.style.color = "blue";
+    display.innerText = e.key + "is keydown";
+});
+
+//adding a keyup event listener to the document
+document.addEventListener("keyup", function(e) {
+    display.style.color = "yellow";
+    display.innerText = e.key + "is keyup";
+});
